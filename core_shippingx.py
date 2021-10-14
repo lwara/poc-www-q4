f82f36d70921ede8d0488e1577c36319611794dd
zio-git <73478973+zio-git@users.noreply.github.com>
2021-10-14 17:31:04 +0200

    cluster 5

--- core_shippingx.py
+++ core_shippingx.py
@@ -30,7 +30,7 @@ def alert(url, params):
 recipients = ["+265998006237", "+265991450316", "+265995246144", "+265999611280", "+265994427700", "+265992600883", "+265999679975"]
 
 #* Get cluster details
-cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/31')
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/5')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
@@ -58,7 +58,7 @@ for site_id in cluster['site']:
 
             # send sms alert
             for recipient in recipients:
-                msg = "Hi there,\n\nDeployment of CORE to v4.9.0 for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
+                msg = "Hi there,\n\nDeployment of CORE to v4.9.1 for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
                 params = {
                     "api_key": os.getenv('API_KEY'),
                     "recipient": recipient,
@@ -75,7 +75,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of CORE to v4.9.0 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
+                    msg = "Hi there,\n\nDeployment of CORE to v4.9.1 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
