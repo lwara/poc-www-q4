eb2c386e19925b9d57bf66eb00d31441f573c6ca
Billz <58287516+billz94@users.noreply.github.com>
2021-10-16 16:18:45 +0200

    Update art_shippingx.py
    cluster 19

--- art_shippingx.py
+++ art_shippingx.py
@@ -28,9 +28,9 @@ def alert(url, params):
     return r
 
 
-recipients = ["+265998006237", "+265991450316", "+265995246144", "+265999611280", "+265995440499"] 
+recipients = ["+265998006237", "+265991450316", "+265995246144", "+265995628519", "+265999959499", "+265881262319", "+265882233319"] 
 
-cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/12')
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/19')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
